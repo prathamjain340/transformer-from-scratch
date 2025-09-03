bd011c59b9a12e54c2333058954a1d0195dd7ceb
Pratham <prathamjain340@gmail.com>
2025-09-03 17:00:17 +0530

    cleaned the code and added comments (start of part 3)

--- bigram_pytorch.py
+++ bigram_pytorch.py
@@ -1,5 +1,6 @@
 import torch
 import torch.nn.functional as F
+import random
 
 # read in all words
 words = open("names.txt", "r").read().splitlines()
@@ -9,43 +10,66 @@ char = sorted(set(''.join(words)))
 stoi = {s:i+1 for i, s in enumerate(char)}
 stoi["."] = 0
 itos = {i:s for s, i  in stoi.items()}
+vocab_size = len(itos)
 
 # build the dataset
 block_size = 3 # context length: how many characters do we take to predict the next one
-X, Y = [], []
-for w in words:
-    context = [0] * block_size
-    for ch in w + '.':
-        ix = stoi[ch]
-        X.append(context)
-        Y.append(ix)
-        context = context[1:] + [ix]
 
-X = torch.tensor(X)
-Y = torch.tensor(Y)
+def build_dataset(words):
+    X, Y = [], []
+    for w in words:
+        context = [0] * block_size
+        for ch in w + '.':
+            ix = stoi[ch]
+            X.append(context)
+            Y.append(ix)
+            context = context[1:] + [ix] # crop and append
 
-C = torch.randn((27, 10))
-W1 = torch.randn(30, 200)
-b1 = torch.randn(200)
-W2 = torch.randn(200, 27)
-b2 = torch.randn(27)
+    X = torch.tensor(X)
+    Y = torch.tensor(Y)
+    return X, Y
+
+random.seed(42)
+random.shuffle(words)
+n1 = int(0.8*len(words))
+n2 = int(0.9*len(words))
+
+Xtr, Ytr = build_dataset(words[:n1])        # 80%
+Xdev, Ydev = build_dataset(words[n1:n2])    # 10%
+Xte, Yte = build_dataset(words[n2:])        # 10%
+
+# MLP
+n_embd = 10 # the dimensionality of the character embedding vectors
+n_hidden = 200 # the number of neurons in the hidden layer of the MLP
+
+C = torch.randn((vocab_size, n_embd))
+W1 = torch.randn(n_embd * block_size, n_hidden)
+b1 = torch.randn(n_hidden)
+W2 = torch.randn(n_hidden, vocab_size)
+b2 = torch.randn(vocab_size)
+
 parameters = [C, W1, b1, W2, b2]
-parametersnum = sum(p.nelement() for p in parameters)
-print(f'Num of parameters = {parametersnum}')
+parasum = sum(p.nelement() for p in parameters) # total number of parameters
+print(f'Num of parameters = {parasum}')
 
 for p in parameters:
     p.requires_grad = True
 
 epochs = 100000
+batch_size = 32
+
 for i in range(epochs):
     # mini batch construct
-    ix = torch.randint(0, X.shape[0], (32,))
+    ix = torch.randint(0, Xtr.shape[0], (batch_size,))
+    Xb, Yb = Xtr[ix], Ytr[ix] # batch X, Y
 
     # forward pass
-    emb = C[X[ix]] # (32, 3, 2)
-    h = torch.tanh(emb.view(-1, 30) @ W1 + b1) # (32, 100)
-    logits = h @ W2 + b2 # (32, 27)
-    loss = F.cross_entropy(logits, Y[ix])
+    emb = C[Xb[ix]] # Embed the characters into vectors
+    embcat = emb.view(emb.shape[0], -1) # concatenate the vectors
+    hpreact = embcat @ W1 + b1 # hidden layer pre-activation
+    h = torch.tanh(hpreact) # hidden layer
+    logits = h @ W2 + b2 # output layer
+    loss = F.cross_entropy(logits, Yb) # loss function
 
     # backward pass
     for p in parameters:
@@ -53,24 +77,46 @@ for i in range(epochs):
     loss.backward()
 
     # update
-    lr = 0.1 if i < epochs/2 else 0.01
+    lr = 0.1 if i < epochs/2 else 0.01 # step learning rate decay
     for p in parameters:
         p.data += -lr * p.grad
 
+@torch.no_grad() # this decorator disables gradient tracking
+def split_loss(split):
+    x, y = {
+        'train' : (Xtr, Ytr),
+        'val' : (Xdev, Ydev),
+        'test' : (Xte, Yte)
+    }[split]
+    emb = C[x] # (N, block_size, n_embd)
+    embcat = emb.view(emb.shape[0], -1) # concat into (N, block_size * n_embd)
+    h = torch.tanh(embcat @ W1 + b1) # (N, n_hidden)
+    logits = h @ W2 + b2 # (N, vocab_size)
+    loss = F.cross_entropy(logits, y)
+    print(split, loss.item())
+
+split_loss('train')
+split_loss('val')
+
 print(f'loss: {loss.item()}')
 
+# sample from the modelx
 for _ in range(20):
     out = []
-    context = [0] * block_size
+    context = [0] * block_size # initialize with all ...
     while True:
-        emb = C[torch.tensor([context])]
+        # forward pass the neural net
+        emb = C[torch.tensor([context])] # (1, block_size, n_embd   )
         h = torch.tanh(emb.view(1, -1) @ W1 + b1)
         logits = h @ W2 + b2
         probs = F.softmax(logits, dim = 1)
+        # sample from the distribution
         ix = torch.multinomial(probs, num_samples=1).item()
+        # shift the context window and track the samples
         context = context[1:] + [ix]
         out.append(ix)
+        # if we sample '.' token, break
         if ix == 0:
             break
 
-    print(''.join(itos[i] for i in out))
+    print(''.join(itos[i] for i in out)) # decode and print the generated word
