34ac71691d7ca76d2eb2c7fbd6e78716f70f3883
Pratham <prathamjain340@gmail.com>
2025-09-03 12:58:31 +0530

    Optimised by increasing model size (parameters), and generated name like names

--- bigram_pytorch.py
+++ bigram_pytorch.py
@@ -14,45 +14,37 @@ itos = {i:s for s, i  in stoi.items()}
 block_size = 3 # context length: how many characters do we take to predict the next one
 X, Y = [], []
 for w in words:
-    # print(w)
     context = [0] * block_size
     for ch in w + '.':
         ix = stoi[ch]
         X.append(context)
         Y.append(ix)
-        # print(''.join(itos[i] for i in context), '----->', itos[ix])
         context = context[1:] + [ix]
 
 X = torch.tensor(X)
 Y = torch.tensor(Y)
 
-C = torch.randn((27, 2))
-W1 = torch.randn(6, 100)
-b1 = torch.randn(100)
-W2 = torch.randn(100, 27)
+C = torch.randn((27, 10))
+W1 = torch.randn(30, 200)
+b1 = torch.randn(200)
+W2 = torch.randn(200, 27)
 b2 = torch.randn(27)
 parameters = [C, W1, b1, W2, b2]
+parametersnum = sum(p.nelement() for p in parameters)
+print(f'Num of parameters = {parametersnum}')
 
 for p in parameters:
     p.requires_grad = True
 
-# lre = torch.linspace(-3, 0, 1000)
-# lrs = 10**lre
-
-# lri = []
-# lossi = []
-
-for i in range(10000):
+epochs = 100000
+for i in range(epochs):
     # mini batch construct
     ix = torch.randint(0, X.shape[0], (32,))
 
     # forward pass
     emb = C[X[ix]] # (32, 3, 2)
-    h = torch.tanh(emb.view(-1, 6) @ W1 + b1) # (32, 100)
+    h = torch.tanh(emb.view(-1, 30) @ W1 + b1) # (32, 100)
     logits = h @ W2 + b2 # (32, 27)
-    # counts = logits.exp()
-    # probs = counts / counts.sum(1, keepdims=True)
-    # loss = -probs[torch.arange(32), Y].log().mean()
     loss = F.cross_entropy(logits, Y[ix])
 
     # backward pass
@@ -61,13 +53,24 @@ for i in range(10000):
     loss.backward()
 
     # update
-    # lr = lrs[i]
-    lr = 0.01
+    lr = 0.1 if i < epochs/2 else 0.01
     for p in parameters:
         p.data += -lr * p.grad
 
-    # track stats
-    # lri.append(lre[i])
-    # lossi.append(loss.item())
-
 print(f'loss: {loss.item()}')
+
+for _ in range(20):
+    out = []
+    context = [0] * block_size
+    while True:
+        emb = C[torch.tensor([context])]
+        h = torch.tanh(emb.view(1, -1) @ W1 + b1)
+        logits = h @ W2 + b2
+        probs = F.softmax(logits, dim = 1)
+        ix = torch.multinomial(probs, num_samples=1).item()
+        context = context[1:] + [ix]
+        out.append(ix)
+        if ix == 0:
+            break
+
+    print(''.join(itos[i] for i in out))
